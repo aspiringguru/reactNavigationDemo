028d3674aa91ea6bf7d82928fd9bdf9a96f7ea9d
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-27 14:44:24 +1000

    added screens, hardcoded text, duplicate screens

--- index.android.js
+++ index.android.js
@@ -1,13 +1,16 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+*
+*   https://reactnavigation.org/docs/navigators/stack
+*
+*
  */
 
 import React from 'react';
 import {
   AppRegistry,
   Text,
+  View,
+  Button,
 } from 'react-native';
 
 import { StackNavigator } from 'react-navigation';
@@ -27,10 +30,29 @@ class HomeScreen extends React.Component {
     title: 'Welcome',
   };
   render() {
-    return <Text>Hello, Navigation!</Text>;
-  }
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+        <Text>---------------</Text>
+        <Button
+          onPress={() => navigate('Chat2')}
+          title="Chat with Wendy"
+        />
+      </View>
+    )
+  }/* end render */
 }/* end class HomeScreen extends React.Component */
 
+
+/*
+*     2nd screen added for demo purposes.
+**    todo : move this to an external file and import
+*/
 class HomeScreen2 extends React.Component {
   static navigationOptions = {
     title: 'WelcomeTwo',
@@ -40,9 +62,37 @@ class HomeScreen2 extends React.Component {
   }
 }/* end class HomeScreen2 extends React.Component */
 
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}/* end class ChatScreen extends React.Component */
+
+class ChatScreen2 extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Wendy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Wendy</Text>
+      </View>
+    );
+  }
+}/* end class ChatScreen2 extends React.Component */
+
 
 const SimpleApp = StackNavigator({
-  Home: { screen: HomeScreen2 },
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
+  Chat2: { screen: ChatScreen2 },
 });
 
 AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
